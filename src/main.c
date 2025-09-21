311a216e19c7e49d16e1f06a48f4df3028fdc204
smartkre <smartkreshuk@gmail.com>
2025-09-21 20:56:31 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,13 +10,40 @@
 #define LED_PORT GPIOC
 #define LED_PIN  (1 << 13)      // PC13
 
+// --- Инициализация тактирования с внешним HSE (8 МГц) и отключением LSE ---
 void System_Init(void) {
-    // Включение HSI
+    // Включение HSI для стабильного старта
     RCC->CR |= RCC_CR_HSION;
-    while ((RCC->CR & RCC_CR_HSIRDY) == 0);
-    
-    // Включение тактирования портов, таймеров и AFIO
-    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN | RCC_APB2ENR_AFIOEN;
+    while (!(RCC->CR & RCC_CR_HSIRDY));
+
+    // Включение Backup Domain для LSE
+    RCC->APB1ENR |= RCC_APB1ENR_BKPEN | RCC_APB1ENR_PWREN;
+    // Отключение LSE (32.768 кГц)
+    RCC->BDCR &= ~RCC_BDCR_LSEON;
+    while (RCC->BDCR & RCC_BDCR_LSERDY);
+
+    // Включение HSE (внешний кварц 8 МГц)
+    RCC->CR |= RCC_CR_HSEON;
+    while (!(RCC->CR & RCC_CR_HSERDY));
+
+    // Отключение PLL
+    RCC->CR &= ~RCC_CR_PLLON;
+    while (RCC->CR & RCC_CR_PLLRDY);
+
+    // Переключение SYSCLK на HSE
+    RCC->CFGR &= ~RCC_CFGR_SW;
+    RCC->CFGR |= RCC_CFGR_SW_HSE;
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE);
+
+    // Сброс делителей
+    RCC->CFGR &= ~(RCC_CFGR_HPRE | RCC_CFGR_PPRE1 | RCC_CFGR_PPRE2);
+
+    // Настройка Flash Latency (0WS для 0-24 МГц)
+    FLASH->ACR &= ~FLASH_ACR_LATENCY;
+    FLASH->ACR |= FLASH_ACR_LATENCY_0;
+
+    // Включаем тактирование GPIOA, GPIOB, GPIOC, AFIO и TIM3
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN | RCC_APB2ENR_AFIOEN;
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 }
 
@@ -28,13 +55,11 @@ void GPIO_Init(void) {
     
     // Настройка PB4 (TIM3_CH1) как Alternate Function Push-Pull
     GPIOB->CRL &= ~(GPIO_CRL_MODE4 | GPIO_CRL_CNF4);
-    GPIOB->CRL |= GPIO_CRL_MODE4_0 | GPIO_CRL_MODE4_1; // Output mode, 50 MHz
-    GPIOB->CRL |= GPIO_CRL_CNF4_1; // Alternate function output Push-pull
+    GPIOB->CRL |= (0xB << (4 * 4)); // AF push-pull 10MHz (аналогично PA2 из вашего кода)
     
     // Настройка PB1 (TIM3_CH4) как Alternate Function Push-Pull
     GPIOB->CRL &= ~(GPIO_CRL_MODE1 | GPIO_CRL_CNF1);
-    GPIOB->CRL |= GPIO_CRL_MODE1_0 | GPIO_CRL_MODE1_1; // Output mode, 50 MHz
-    GPIOB->CRL |= GPIO_CRL_CNF1_1; // Alternate function output Push-pull
+    GPIOB->CRL |= (0xB << (1 * 4)); // AF push-pull 10MHz
     
     // Переназначение TIM3_CH4 на PB1 (по умолчанию на PC9)
     AFIO->MAPR |= AFIO_MAPR_TIM3_REMAP_0; // Partial remap: TIM3_CH4 -> PB1
@@ -53,13 +78,13 @@ void TIM3_Init_500kHz(void) {
     TIM3->CCR4 = 0;
     
     // Настройка таймера для генерации 500 kHz на CH1
-    TIM3->PSC = 7;          // Делитель: 8 MHz / (7 + 1) = 1 MHz
-    TIM3->ARR = 1;          // Период: 2 такта (1 MHz / 2 = 500 kHz)
-    TIM3->CCR1 = 1;         // Скважность 50%
+    TIM3->PSC = 0;          // Без деления (частота таймера = 8 МГц)
+    TIM3->ARR = 15;         // Период = 16 / 8 МГц = 500 kHz (8MHz / 16 = 500kHz)
+    TIM3->CCR1 = 8;         // Скважность 50%
     
     // Настройка канала 1 в PWM mode 1
     TIM3->CCMR1 &= ~TIM_CCMR1_OC1M;
-    TIM3->CCMR1 |= (6 << 4); // PWM mode 1 (OC1M = 110)
+    TIM3->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; // PWM Mode 1
     TIM3->CCMR1 |= TIM_CCMR1_OC1PE; // Enable preload
     
     // Выключаем канал 4
@@ -85,13 +110,13 @@ void TIM3_Init_200kHz(void) {
     TIM3->CCR4 = 0;
     
     // Настройка таймера для генерации 200 kHz на CH4
-    TIM3->PSC = 7;          // Делитель: 8 MHz / (7 + 1) = 1 MHz
-    TIM3->ARR = 4;          // Период: 5 тактов (1 MHz / 5 = 200 kHz)
-    TIM3->CCR4 = 2;         // Скважность 50%
+    TIM3->PSC = 0;          // Без деления (частота таймера = 8 МГц)
+    TIM3->ARR = 39;         // Период = 40 / 8 МГц = 200 kHz (8MHz / 40 = 200kHz)
+    TIM3->CCR4 = 20;        // Скважность 50%
     
     // Настройка канала 4 в PWM mode 1
     TIM3->CCMR2 &= ~TIM_CCMR2_OC4M;
-    TIM3->CCMR2 |= (6 << 12); // PWM mode 1 (OC4M = 110)
+    TIM3->CCMR2 |= TIM_CCMR2_OC4M_1 | TIM_CCMR2_OC4M_2; // PWM Mode 1
     TIM3->CCMR2 |= TIM_CCMR2_OC4PE; // Enable preload
     
     // Выключаем канал 1
