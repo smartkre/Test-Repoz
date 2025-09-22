4889f7d9e7b8fc31fb41b33afd8d718515f83e87
smartkre <smartkreshuk@gmail.com>
2025-09-22 20:10:09 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
+#define STM32F103x6
 #include "stm32f1xx.h"
 
 #define LED_PORT GPIOC
-#define LED_PIN  13
+#define LED_PIN  (1 << 13)   // PC13
 
 #define SCI_PORT GPIOB
-#define SCI_PIN  4
+#define SCI_PIN  (1 << 4)    // PB4
 
 static void delay(volatile uint32_t d) {
     while (d--) __asm__("nop");
@@ -15,26 +16,26 @@ int main(void) {
     RCC->APB2ENR |= RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN;
 
     /* Настраиваем PC13 как push-pull output (LED) */
-    LED_PORT->CRH &= ~(0xF << ((LED_PIN - 8) * 4));
-    LED_PORT->CRH |=  (0x1 << ((LED_PIN - 8) * 4)); // 0b0001 = output, max 10 MHz
+    GPIOC->CRH &= ~(0xF << ((13 - 8) * 4));
+    GPIOC->CRH |=  (0x1 << ((13 - 8) * 4)); // 0b0001 = Output, max 10 MHz, push-pull
 
     /* Настраиваем PB4 как push-pull output */
-    SCI_PORT->CRL &= ~(0xF << (SCI_PIN * 4));
-    SCI_PORT->CRL |=  (0x3 << (SCI_PIN * 4)); // 0b0011 = output, max 50 MHz
+    GPIOB->CRL &= ~(0xF << (4 * 4));
+    GPIOB->CRL |=  (0x3 << (4 * 4));        // 0b0011 = Output, max 50 MHz, push-pull
 
     /* Генерируем 1000 импульсов на PB4 */
     for (int i = 0; i < 1000; i++) {
-        SCI_PORT->BSRR = (1 << SCI_PIN);  // high
-        delay(100);                       // задержка
-        SCI_PORT->BRR  = (1 << SCI_PIN);  // low
-        delay(100);                       // задержка
+        GPIOB->BSRR = SCI_PIN;  // high
+        delay(100);
+        GPIOB->BRR  = SCI_PIN;  // low
+        delay(100);
     }
 
     /* Основной цикл: мигаем светодиодом */
     while (1) {
-        LED_PORT->BRR  = (1 << LED_PIN); // LED on
+        GPIOC->BRR  = LED_PIN;  // LED ON (PC13 низкий уровень)
         delay(500000);
-        LED_PORT->BSRR = (1 << LED_PIN); // LED off
+        GPIOC->BSRR = LED_PIN;  // LED OFF (PC13 высокий уровень)
         delay(500000);
     }
 }
